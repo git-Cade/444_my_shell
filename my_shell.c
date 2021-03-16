922efb73b90397fa0081f04e61a6ff94f99bf7ae
CadeLilley <cadelilley@gmail.com>
2021-03-16 15:02:31 -0700

    Initial commit

---
+++ my_shell.c
@@ -0,0 +1,112 @@
+// Cade Lilley
+// Lab03A - Simple shell
+// Leaves no child processes behind
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <limits.h>
+
+#define MAX_INPUT_SIZE 1024
+#define MAX_TOKEN_SIZE 64
+#define MAX_NUM_TOKENS 64
+
+/* Splits the string by space and returns the array of tokens
+*
+*/
+char **tokenize(char *line)
+{
+  char **tokens = (char **)malloc(MAX_NUM_TOKENS * sizeof(char *));
+  char *token = (char *)malloc(MAX_TOKEN_SIZE * sizeof(char));
+  int i, tokenIndex = 0, tokenNo = 0;
+
+  for(i =0; i < strlen(line); i++){
+
+    char readChar = line[i];
+
+    if (readChar == ' ' || readChar == '\n' || readChar == '\t'){
+      token[tokenIndex] = '\0';
+      if (tokenIndex != 0){
+	tokens[tokenNo] = (char*)malloc(MAX_TOKEN_SIZE*sizeof(char));
+	strcpy(tokens[tokenNo++], token);
+	tokenIndex = 0; 
+      }
+    } else {
+      token[tokenIndex++] = readChar;
+    }
+  }
+ 
+  free(token);
+  tokens[tokenNo] = NULL ;
+  return tokens;
+}
+
+
+int main(int argc, char* argv[]) {
+	char  line[MAX_INPUT_SIZE];            
+	char  **tokens;              
+	int i;
+
+	FILE* fp;
+	if(argc == 2) {
+		fp = fopen(argv[1],"r");
+		if(fp < 0) {
+			printf("File doesn't exists.");
+			return -1;
+		}
+	}
+
+	while(1) {			
+		/* BEGIN: TAKING INPUT */
+		bzero(line, sizeof(line));
+		if(argc == 2) { // batch mode
+			if(fgets(line, sizeof(line), fp) == NULL) { // file reading finished
+				break;	
+			}
+			line[strlen(line) - 1] = '\0';
+		} else { // interactive mode
+			printf("$ ");
+			scanf("%[^\n]", line);
+			getchar();
+		}
+		/* END: TAKING INPUT */
+
+		line[strlen(line)] = '\n'; //terminate with new line
+		tokens = tokenize(line);
+
+		char* usr_bin_str = malloc(1000);
+		strcpy(usr_bin_str, "/usr/bin/");
+
+		if (tokens[0] == NULL) { // No input
+			continue;
+		} else if (!strcmp(tokens[0], "cd")) { // If command is cd, change parent cwd
+			if(chdir(tokens[1])) {
+				perror("cd failed");
+			}
+			char cwd[PATH_MAX];
+   			getcwd(cwd, sizeof(cwd));
+   			printf("Current Working Directory: %s\n", cwd);
+		} else if (access(strcat(usr_bin_str, tokens[0]), F_OK)) { // If command doesn't exist
+			printf("Command not found\n");
+		//} else if (strlen(line) > 1) { // Any other basic command
+		} else {
+			int ppid = fork();
+			if (!ppid) {
+				execvp(tokens[0], tokens);  
+			} else {
+				wait(NULL); // Waits for child process to finish
+			}
+		}
+
+		// Freeing the allocated memory	
+		for(i=0;tokens[i]!=NULL;i++){
+			free(tokens[i]);
+		}
+		free(tokens);
+
+	}
+	return 0;
+}
